ce9be8bbc384666e0740336e8a329b3b243890f9
Warren Dukes <warren.dukes@gmail.com>
2004-02-24 16:21:49 +0000

    clean up stuff
    git-svn-id: https://svn.musicpd.org/mpc/trunk@27 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ echo "Generating build information using aclocal, automake and autoconf"
 echo "This may take a while ..."
 
 # Touch the timestamps on all the files since CVS messes them up
-touch Makefile.am configure.in
+#touch Makefile.am configure.ac
 
 rm -f configure
 rm -f config.cache
