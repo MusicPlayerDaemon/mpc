70c02913f09650e4013912d8f5757562ff2a583d
Eric Wong <normalperson@yhbt.net>
2006-06-04 08:31:18 +0000

    Reject r4232
    This breaks scripting. (mpc listall |grep foo | mpc add)
    > Incorrect character encoding in first line of 'mpc status' output
    >
    > MPD gives report about songs tags in UTF8 charset. XTerm has
    > KOI8-R locale.  Some symbols can not be converted from UTF8 to
    > KOI8. For example -- '№'.
    > mpc use libiconv for character conversation.
    > Now, some tags, what have invalid (for a target
    > charset) symbols, is not converted (by mpc) and displayed on
    > KOI8-R terminal in native UTF8 coding (aka odd hieroglyphs for
    > terminal users).
    > This is bug in charConv.c file.
    >
    > First way for solving of the bug is to use libiconv IGNORE
    > parameter for character set conversion setting like in
    > iconv_prog.c (i.e. to emulate iconv -c parameter).
    > The patch for that way is attached. I have tested this patch on
    > my system and mpc from now works good.
    >
    > Second way (and better, I think), is to use librecord instead of libiconv.
    > For example:
    >    original text "Моцарт -- Симфония №40" ("Mozart - Symphony №40").
    >    iconv -c text "Моцарт -- Симфония 40" ("Mozart - Symphony 40").
    >    recode   text "Моцарт -- Симфония No40" ("Mozart - Symphony No40").
    > I believe, that replacing '№' to 'No' is definitely better
    > than simply to drop that symbol from output.
    
    git-svn-id: https://svn.musicpd.org/mpc/trunk@4241 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/charConv.c
+++ src/charConv.c
@@ -45,33 +45,6 @@ static void closeCharSetConversion();
 
 static int setCharSetConversion(char * to, char * from) {
 #ifdef HAVE_ICONV
-	/* begin code from iconv_prog.c (omiting invalid symbols) */
-	const char *errhand = strchrnul (to, '/');
-	int nslash = 2;
-	char *newp = NULL;
-	char *cp;
-
-	if (*errhand == '/') {
-		--nslash;
-		errhand = strchrnul (errhand, '/');
-
-		if (*errhand == '/') {
-			--nslash;
-			errhand = strchr(errhand, '\0');
-		}
-	}
-
-	newp = (char *)malloc(errhand - to + nslash + 7 + 1);
-	cp = mempcpy (newp, to, errhand - to);
-	while (nslash-- > 0)
-		*cp++ = '/';
-	if (cp[-1] != '/')
-		*cp++ = ',';
-	memcpy (cp, "IGNORE", sizeof ("IGNORE"));
-
-	to = newp;
-	/* end code from iconv_prog.c */
-
 	if(char_conv_to && strcmp(to,char_conv_to)==0 &&
 			char_conv_from && strcmp(from,char_conv_from)==0)
 		return 0;
@@ -83,9 +56,6 @@ static int setCharSetConversion(char * to, char * from) {
 
 	char_conv_to = strdup(to);
 	char_conv_from = strdup(from);
-
-	if (newp)
-		free(newp);
 
 	return 0;
 #endif
