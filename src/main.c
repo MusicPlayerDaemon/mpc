f45510ea8a3edbcb7d67eb4fcb426351b5044eff
Michael Gehring <mg@ebfe.org>
2011-05-28 04:15:14 +0200

    command/sticker: fix min. number of arguments
    'mpc sticker foo' caused a segfault by dereferencing argv[1] in
    cmd_sticker().

--- src/main.c
+++ src/main.c
@@ -105,7 +105,7 @@ static struct command {
 #endif
 	{"update",      0,   -1,  2,    cmd_update,      "[<path>]", "Scan music directory for updates"},
 #if LIBMPDCLIENT_CHECK_VERSION(2,1,0)
-	{"sticker",     1,   -1,  0,    cmd_sticker,     "<uri> <get|set|list|del> <args..>", "Sticker management"},
+	{"sticker",     2,   -1,  0,    cmd_sticker,     "<uri> <get|set|list|del> <args..>", "Sticker management"},
 #endif
 	{"stats",       0,   -1,  0,    cmd_stats,       "", "Display statistics about MPD"},
 	{"version",     0,   0,   0,    cmd_version,     "", "Report version of MPD"},
