2a710cb6600176a2cb4b4c185e895678048955a4
Max Kellermann <max@duempel.org>
2013-11-09 18:30:38 +0100

    new copyright header
    Updated year, no individual names, improved layout.  Mostly copied
    from MPD.

--- src/list.c
+++ src/list.c
@@ -1,24 +1,22 @@
-/* music player command (mpc)
- * Copyright (C) 2003-2008 Warren Dukes <warren.dukes@gmail.com>,
-				Eric Wong <normalperson@yhbt.net>,
-				Daniel Brown <danb@cs.utexas.edu>
- * Copyright (C) 2008-2010 Max Kellermann <max@duempel.org>
- * Project homepage: http://musicpd.org
- 
+/*
+ * music player command (mpc)
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ */
 
 #include "list.h"
 
--- src/sticker.c
+++ src/sticker.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2008-2010 The Music Player Daemon Project
+ * music player command (mpc)
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
