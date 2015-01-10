aef26d4218039dad4c101b51d3d213c7eba53f81
Max Kellermann <max@duempel.org>
2015-01-10 09:01:38 +0100

    Copyright year 2015

--- src/format.h
+++ src/format.h
@@ -1,6 +1,6 @@
 /*
  * music player command (mpc)
- * Copyright (C) 2003-2014 The Music Player Daemon Project
+ * Copyright (C) 2003-2015 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
