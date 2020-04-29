MusicPlayerDaemon/mpc

commit 8d83ecd5c4a76d3216641a6a3db90697fc8a0dae

search: convert local variable to enum
Eliminates two implicit casts.

src/search.c
@@ -77,7 +77,6 @@ static int
 get_constraints(int argc, char **argv, struct constraint **constraints)
 {
 	int numconstraints = 0;
-	int type;
 	int i;
 
 	assert(argc > 0 && argc % 2 == 0);
@@ -85,8 +84,8 @@ get_constraints(int argc, char **argv, struct constraint **constraints)
 	*constraints = malloc(sizeof(**constraints) * argc / 2);
 
 	for (i = 0; i < argc; i += 2) {
-		type = get_search_type(argv[i]);
-		if (type < 0) {
+		enum mpd_tag_type type = get_search_type(argv[i]);
+		if (type != MPD_TAG_UNKNOWN) {
 			free(*constraints);
 			return -1;
 		}

commit 029be387d2bd85e8b34269d2bf057c7f3c96d68d

search: fix inverted check
Regression from commit 8d83ecd5c4a76d3216641a6a3db90697fc8a0dae

src/search.c
@@ -84,7 +84,7 @@ get_constraints(int argc, char **argv, struct constraint **constraints)
 
 	for (int i = 0; i < argc; i += 2) {
 		enum mpd_tag_type type = get_search_type(argv[i]);
-		if (type != MPD_TAG_UNKNOWN) {
+		if (type == MPD_TAG_UNKNOWN) {
 			free(*constraints);
 			return -1;
 		}