5067f9acccb43db797e5d577381ef7a0f3d0f3f1
Max Kellermann <max@musicpd.org>
2021-05-18 14:02:37 +0200

    Copyright 2021

--- src/queue.c
+++ src/queue.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2003-2008 Warren Dukes <warren.dukes@gmail.com>,
 				Eric Wong <normalperson@yhbt.net>,
 				Daniel Brown <danb@cs.utexas.edu>
- * Copyright (C) 2008-2020 Max Kellermann <max@duempel.org>
+ * Copyright (C) 2008-2021 Max Kellermann <max@duempel.org>
  * Project homepage: http://musicpd.org
 
  * This program is free software; you can redistribute it and/or modify
