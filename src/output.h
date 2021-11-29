5898fe514315fd58f10eb519ce644aa26db61ca2
Max Kellermann <max@musicpd.org>
2021-11-29 18:45:29 +0100

    output: do not include mpd/version.h
    Including headers other than mpd/client.h is not supported, and
    <mpd/version.h> will break when building libmpdclient as Meson
    submodule.

--- src/output.h
+++ src/output.h
@@ -21,8 +21,6 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
-#include <mpd/version.h>
-
 struct mpd_connection;
 
 int cmd_enable( int argc, char **argv, struct mpd_connection *conn);
@@ -33,11 +31,7 @@ cmd_toggle_output(int argc, char **argv, struct mpd_connection *conn);
 
 int cmd_outputs(int argc, char **argv, struct mpd_connection *conn);
 
-#if LIBMPDCLIENT_CHECK_VERSION(2,14,0)
-
 int
 cmd_outputset(int argc, char **argv, struct mpd_connection *conn);
-
-#endif
 
 #endif
