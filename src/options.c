MusicPlayerDaemon/mpc

commit 806ba2edd7e116fda1036294ecab25fe21375db3

options: support options without "shortopt"

src/options.c
@@ -52,7 +52,7 @@ struct Options options = {
 static const struct OptionDef option_table[] = {
 	{ 'v', "verbose", NULL, "Give verbose output" },
 	{ 'q', "quiet", NULL, "Suppress status message" },
-	{ 'q', "no-status", NULL, "synonym for --quiet" },
+	{ 0, "no-status", NULL, "synonym for --quiet" },
 	{ 'h', "host", "<host>", "Connect to server on <host>" },
 	{ 'P', "password", "<password>", "Connect to server using password <password>" },
 	{ 'p', "port", "<port>", "Connect to server port <port>" },
@@ -186,14 +186,20 @@ void
 print_option_help(void)
 {
 	for (unsigned i = 0; i < option_table_size; i++) {
-		printf("  -%c, ", option_table[i].shortopt);
+		int remaining = 28;
+		if (option_table[i].shortopt) {
+			printf("  -%c, ", option_table[i].shortopt);
+			remaining -= 4;
+		} else
+			printf("  ");
+
 		if (option_table[i].argument)
 			printf("--%s=%-*s",
 			       option_table[i].longopt,
-			       25 - (int) strlen(option_table[i].longopt),
+			       remaining - (int) strlen(option_table[i].longopt),
 			       option_table[i].argument);
 		else
-			printf("--%-25s ", option_table[i].longopt);
+			printf("--%-*s ", remaining, option_table[i].longopt);
 		printf("%s\n", option_table[i].description);
 	}
 }

commit 044bfe506eed655952385eb59fadb022c8927db5

options: fix --no-status
Partial revert of 806ba2edd7e116fda1036294ecab25fe21375db3, which
introduced a bug described in issue #108.

src/options.c
@@ -40,7 +40,7 @@ struct Options options = {
 static const struct OptionDef option_table[] = {
 	{ 'v', "verbose", NULL, "Give verbose output" },
 	{ 'q', "quiet", NULL, "Suppress status message" },
-	{ 0, "no-status", NULL, "synonym for --quiet" },
+	{ 'q', "no-status", NULL, "synonym for --quiet" },
 	{ 'h', "host", "<host>", "Connect to server on <host>" },
 	{ 'P', "password", "<password>", "Connect to server using password <password>" },
 	{ 'p', "port", "<port>", "Connect to server port <port>" },